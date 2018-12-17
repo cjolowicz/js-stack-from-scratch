7287339a18c8dfb0426d9e8786bb1c9909aed4e4
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2018-12-17 14:31:13 +0100

    src/client/component/button.jsx: Fix eslint error react/button-has-type.

--- src/client/component/button.jsx
+++ src/client/component/button.jsx
@@ -8,7 +8,7 @@ type Props = {
 }
 
 const Button = ({ label, handleClick }: Props) => (
-  <button onClick={handleClick}>{label}</button>
+  <button type="button" onClick={handleClick}>{label}</button>
 )
 
 export default Button
