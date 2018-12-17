263149b822a4508c1d9488363136af3f9f00ff84
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2018-12-17 23:29:37 +0100

    src/client/index.jsx: Wrap App component in react-router-dom BrowserRouter.

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -5,6 +5,7 @@ import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import {
   createStore,
@@ -33,9 +34,11 @@ if (rootEl == null) {
 
 const wrapApp = (AppComponent, reduxStore) => (
   <Provider store={reduxStore}>
-    <AppContainer>
-      <AppComponent />
-    </AppContainer>
+    <BrowserRouter>
+      <AppContainer>
+        <AppComponent />
+      </AppContainer>
+    </BrowserRouter>
   </Provider>
 )
 
